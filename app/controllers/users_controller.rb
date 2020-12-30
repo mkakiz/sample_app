cba7a12a725435815957eb955e1ef7010f6e205d
mkakiz <masahiro.kakizaki@gmail.com>
2020-12-30 22:12:37 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new(user_params)
+          #user_params is external method below to initialize hash, used instead of User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App"
+      redirect_to @user   #go to user profile page = redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  
 end
