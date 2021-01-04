fc143c387dfae6cf777f9de0783d4b436054eba1
mkakiz <masahiro.kakizaki@gmail.com>
2021-01-04 01:27:37 +0000

    remember me function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)  #sessions form says email is in session key
     if user && user.authenticate(params[:session][:password])   #checking a user has the email and the password
       log_in user   #log_in is defined in sessions_helper
+      remember user   #remember is in user.rb. put encrypted new_token in remember_digest
       redirect_to user   # same as redirect_to user_url(user), /users/*
     else
       flash.now[:danger] = "Invalid email/password combination"    #.now lets to show flash in rendered page until next action
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
   
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,9 +4,23 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  def remember(user)
+    user.remember   #remember is in user.rb. put new_token in remember_token
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+          #if session exists, set the user as current user
+          #if not, find user from cookie and login
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
   
@@ -14,7 +28,14 @@ module SessionsHelper
     !current_user.nil?   #true if user is logged in
   end
   
+  def forget(user)
+    user.forget  #forget is in user.rb. it delete remember_digest
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+    
   def log_out
+    forget(current_user)   #this forget is here above.
     session.delete(:user_id)
     @current_user = nil
   end
